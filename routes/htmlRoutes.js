81cd4493b57650a5febe08a943ab916c69c8dc18
Splimis <splimis@gmail.com>
2017-12-13 15:12:04 -0500

    initial commit

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = function(app) {
+	// console.log('___ENTER htmlRoutes.js___');
+
+	app.get('/', function(req, res) {
+		res.sendFile(path.join(__dirname, '../views/index.html'));
+	});
+
+	app.get('/contact', function(req, res) {
+		res.sendFile(path.join(__dirname, '../views/contact.html'));
+	});
+
+	app.get('/portfolio', function(req, res) {
+		res.sendFile(path.join(__dirname, '../views/portfolio.html'));
+	});
+};
